878450bcf4621ec6c4c5c29dc4750a7792da5535
Matt Darlington <matthewsdarlington@gmail.com>
2023-07-31 14:59:17 -0600

    Initial commit

---
+++ conditionals_practice.rb
@@ -0,0 +1,25 @@
+# puts 5 == 5
+# puts 5 < 3
+# age = 18
+# if age > 12
+#     puts "Yes, #{age} is greater than 12."
+# end
+
+# name = "Matt"
+# name_2 = "Matt"
+
+# if name == name_2
+#     puts "Yes, #{name} has the same name as #{name_2}."
+# else puts "No, #{name}'s name is not the same as #{name_2}'s name."
+# end
+
+age = 0
+if age >= 21
+    puts "Welcome to the website!"
+elsif age <= 2 && age >= 0
+    puts "How are you even on this device?!?!?"
+elsif age > 2 && age < 21
+    puts "You aren't quite old enough to visit, sorry."
+else
+    puts "You haven't even been born yet!!!"
+end
